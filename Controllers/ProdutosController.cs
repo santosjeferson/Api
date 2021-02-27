636a2899245db307dec13e43096b262db746c3eb
santosjeferson <78939318+santosjeferson@users.noreply.github.com>
2021-02-27 18:23:29 -0300

    Add files via upload

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,39 @@
+﻿using easyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api_teste.Controllers
+{
+    public class ProdutosController : ApiController
+    {
+        private static List<Produtos> produtos = new List<Produtos>();
+
+        public List<Produtos> Get()
+        {
+            return produtos;
+        }
+
+        public void Post(string nome)
+        {   
+            if(!string.IsNullOrEmpty(nome))
+                produtos.Add(new Produtos(nome));
+        }
+
+        public void Delete(string nome)
+        {
+            produtos.RemoveAt(produtos.IndexOf(produtos.First(x => x.Produto.Equals(nome))));
+        }
+
+        public void Put(string nome, string new_value)
+        {
+           
+        produtos.RemoveAt(produtos.IndexOf(produtos.First(x => x.Produto.Equals(nome))));
+        produtos.Add(new Produtos(new_value));  
+
+        }
+    }
+}   
---
+++ Models/Produtos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace easyAPI.Models
+{
+
+    public class Produtos
+    {
+        public string Produto { get; set; }
+
+        public Produtos(string get_dados)
+        {
+            this.Produto = get_dados;
+        }
+
+
+    }
+}
